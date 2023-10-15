230c43888d9bda39c81564378f22adc834edbb91
Pravin Sawant <88920922+IPSCODER@users.noreply.github.com>
2023-10-15 16:48:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const express = require("express");
+const session = require("express-session");
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors({
+  origin:["http://localhost:3000"],
+  methods:["POST","GET"],
+  credentials:true
+}));
+app.use(cookieParser())
+app.use(express.json());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(session({
+  secret:"secret",
+  resave:false,
+  saveUninitialized:false,
+  cookie:{
+    secure:false,
+    maxAge:1000 * 60 * 60 *24
+  }
+}))
+
+
+app.use("/",(req,res)=>{
+    res.send({message:"succufuly running server"})   
 })
-app.listen(process.env.PORT || 3000)
+
+
+app.listen(5100)
